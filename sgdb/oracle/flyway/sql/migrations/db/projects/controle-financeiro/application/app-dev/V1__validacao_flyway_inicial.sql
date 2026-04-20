646fecb678148f21a096e8011d8b4b1d41ce33cd
Welton Magalhães de Oliveira <welton.m.o@hotmail.com>
2026-04-20 01:16:26 -0300

    feat(flyway): add validation configs and initial migrations for app and owner

---
+++ sgdb/oracle/flyway/sql/migrations/db/projects/controle-financeiro/application/app-dev/V1__validacao_flyway_inicial.sql
@@ -0,0 +1,26 @@
+/*=======================================================================================
+
+@versao         : Oracle Database 21c Express Edition Release 21.0.0.0.0 - Production
+@author         : Welton Magalhaes de Oliveira
+@email          : welton.m.o@hotmail.com
+@data-criacao   : 20-04-2026
+@database       : DEV
+
+Descricao:
+Tabela inicial criada apenas para validacao do versionamento com Flyway.
+Esta migration tem como objetivo garantir que a estrutura de migrations,
+configuracao e execucao estejam funcionando corretamente no ambiente DEV.
+
+=======================================================================================*/
+
+-- Tabela de teste inicial (validacao do Flyway)
+CREATE TABLE TB_FLYWAY_TESTE (
+    ID NUMBER PRIMARY KEY,
+    DESCRICAO VARCHAR2(100)
+);
+
+-- Insercao de registro de teste
+INSERT INTO TB_FLYWAY_TESTE (ID, DESCRICAO)
+VALUES (1, 'Tabela de teste inicial (validacao do Flyway) - DEV');
+
+COMMIT;
